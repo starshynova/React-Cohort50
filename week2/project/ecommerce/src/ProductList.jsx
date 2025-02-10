3502062d133c08c83929c9340f4f75f8f8fe91c4
starshynova <l.starshynova@gmail.com>
2025-02-10 12:24:18 +0100

    fix: change (product) to {id, image, title} in ProductList

--- week2/project/ecommerce/src/ProductList.jsx
+++ week2/project/ecommerce/src/ProductList.jsx
@@ -3,12 +3,12 @@ import ProductCard from './ProductCard.jsx';
 const ProductList = ({ filterProducts }) => {
     return (
         <div className="product-list">
-            {filterProducts.map((product) => (
+            {filterProducts.map(({id, image, title}) => (
                 <ProductCard
-                    key={product.id}
-                    id={product.id}
-                    image={product.image}
-                    title={product.title}
+                    key={id}
+                    id={id}
+                    image={image}
+                    title={title}
                 />
             ))}
         </div>
